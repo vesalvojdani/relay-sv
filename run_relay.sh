6693acca8f24d84da4f2cc8942d8f8848e1ce41c
Vesal Vojdani <vesal.vojdani@gmail.com>
2023-10-31 09:32:31 +0000

    Add run_relay script

---
+++ run_relay.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Assume script is called from the relay-radar directory
+RELAY_DIR=$PWD
+
+if [ $# -lt 1 ]; then
+    echo "Usage: run_relay.sh <file_to_analyze>"
+    exit 127
+elif [ "$1" == "--version" ]; then
+    echo "Version: SV-COMP 2024 (v0.10.03)"
+    exit 0
+fi
+
+#Reset server IP to original file content (perhaps not a problem for SV-COMP overlays).
+cp server_ip.bkup server_ip.txt
+
+# 1) "Pick the files to analyze". SV-COMP has only one file to analyze.
+# So create the file "gcc-log.txt" with just the argument file.
+
+tmp_dir=`mktemp -d -p .`
+trap "rm -rf $tmp_dir" EXIT
+cp "$1" "$tmp_dir"
+cd "$tmp_dir"
+echo "duppy -c $1" > gcc-log.txt
+
+# 2) "Pre-process the source files".
+sed -i "s|RELAYROOT=.*|RELAYROOT=$RELAY_DIR|" $RELAY_DIR/scripts/dump-with-stats.sh
+$RELAY_DIR/scripts/dump-with-stats.sh
+
+
+# 3) Run the analysis
+cd ..
+sed -i "s/-u jan/-u $(whoami)/" ./relay.sh
+./relay_single.sh $tmp_dir/ciltrees
